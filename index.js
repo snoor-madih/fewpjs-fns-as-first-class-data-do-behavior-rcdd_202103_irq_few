4596a3cc470f4d9693c2646fa3d9a02a3dbef069
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:27:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-/* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+function greet(timeStr){
+  let nowTime = timeStr.split(':');
+  let nowHour = parseInt(nowTime[0]);
+  if (nowHour<12){
+    return "Good Morning";
+  } else if (nowHour>17){
+    return "Good Evening";
+  }
+  return "Good Afternoon";
 }
-
-/* End Given Code, don't edit above here...*/
-
-/* Write your implementation of greet() */
-
-function greet(time){
-  let time1=time.split(":");
-  let time2=time1[0].concat(time1[1])
-  let lastTime=parseInt(time2,10)
-  if(lastTime<1200)
-  { return "Good Morning";}
-  else if ((lastTime>=1200) && (lastTime<=1700))
-  {return "Good Afternoon";}
-  else  if (lastTime>1700)
-  {return "Good Evening";}
-  else{return "TEST";}
-}
-/* Write your implementation of displayMessage() */
-let message=greet();
-
-function displayMessage(message)
-{
-  
-  let ms=document.getElementById("greeting");
-  ms.innerHTML=message;
+function displayMessage(msg){
+  document.getElementById("greeting").innerText = msg;
 }
